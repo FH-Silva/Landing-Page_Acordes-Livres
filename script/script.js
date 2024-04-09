532cb6526dcf746d3b0ce782e9fd0d1612462390
Fabio Henrique <fabiosilva5210@gmail.com>
2024-04-09 20:26:44 -0300

    commit3

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
-equipe.addEventListener('click', function(){
-    alert("Opção indisponivel no momento");
+var equipe1 = document.querySelectorAll(".equipe"); //Selecionar todos os elementos com essa classe
+var conatos1 = document.querySelectorAll(".contatos");
+
+equipe1.forEach(function(equipe){ //Ele percorre cada elemento do array e executa uma função que você especifica para cada elemento.
+    equipe.addEventListener("click", function(){ //Adiciona um evento quando o usuario clicar
+        alert("Opção indisponivel no momento");
+    });
+});
+
+conatos1.forEach(function(contatos, index){
+    contatos.addEventListener("click", function(){
+        alert("Contatos indisponiveis no momento")
+    });
 });
